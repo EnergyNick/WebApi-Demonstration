d518c550035cc70628c4929330521ad3921e231b
EnergyNick <nsikachin@gmail.com>
2023-05-08 04:30:30 +0500

    Added helper method for pagination

---
+++ UsersManager.Infrastructure/Extensions/QueryableExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace UsersManager.Infrastructure.Extensions;
+
+public static class QueryableExtensions
+{
+    public static IQueryable<TValue> PaginateIfNeeded<TValue>(this IQueryable<TValue> sources,
+        int? size, int? index = null)
+    {
+        index ??= 0;
+        return size == null
+            ? sources
+            : sources.Skip(size.Value * index.Value).Take(size.Value);
+    }
+}
