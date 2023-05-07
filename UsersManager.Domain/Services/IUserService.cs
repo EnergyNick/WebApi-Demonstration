b2d49132b07c76c33b15175620771622fa05a268
EnergyNick <nsikachin@gmail.com>
2023-05-08 04:32:43 +0500

    Create service for user CRD and setting for IOptions
    (CRUD without Update)

---
+++ UsersManager.Domain/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using UsersManager.Domain.Models;
+
+namespace UsersManager.Domain.Services;
+
+public interface IUserService
+{
+    Task<User> CreateUser(CreateUserRequest userInfo, CancellationToken token = default);
+    Task<bool> DeleteUser(string login, CancellationToken token = default);
+
+    Task<User?> GetUser(string login, CancellationToken token = default);
+    Task<IEnumerable<User>> GetUsers(int? pageSize, int? pageIndex, CancellationToken token = default);
+}
---
+++ UsersManager.Domain/ServicesSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersManager.Domain;
+
+public class ServicesSettings
+{
+    public static readonly string SectionName = "Services";
+
+    [Required]
+    public TimeSpan UserCreationTimeout { get; init; }
+}
