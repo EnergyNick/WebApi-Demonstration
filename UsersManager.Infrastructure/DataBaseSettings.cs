32f5ff2c4f951ae7fdb9d648292863584a12299d
EnergyNick <nsikachin@gmail.com>
2023-05-08 04:30:20 +0500

    Added settings for Data Base initialization in IoC

---
+++ UsersManager.Infrastructure/DataBaseSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersManager.Infrastructure;
+
+public record DataBaseSettings
+{
+    public static readonly string SectionName = "DataBase";
+
+    [Required]
+    public string ConnectionString { get; init; }
+}
---
+++ UsersManager.Infrastructure/Extensions/ServicesExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace UsersManager.Infrastructure.Extensions;
+
+public static class ServicesExtensions
+{
+    public static void AddDataBaseInfrastructure(this IServiceCollection services)
+    {
+        services.AddDbContextPool<UsersContext>((provider, builder) =>
+        {
+            var options = provider.GetService<IOptions<DataBaseSettings>>();
+            var cfg = options?.Value
+                ?? throw new InvalidOperationException("Can't create database context without connection settings");
+
+            builder.UseNpgsql(cfg.ConnectionString);
+        });
+    }
+}
