9ff7322b6b65c130b10756263b2d68d54bb3d644
EnergyNick <nsikachin@gmail.com>
2023-05-08 05:00:30 +0500

    Added utilities tools for service configuration

---
+++ UsersManager.Service/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+
+namespace UsersManager.Service.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void AddLoggingProvider(this WebApplicationBuilder builder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        builder.Host.UseSerilog();
+    }
+
+    public static void AddOptionWithValidate<T>(this IServiceCollection services, string sectionName)
+        where T : class
+    {
+        services.AddOptions<T>()
+            .BindConfiguration(sectionName)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+    }
+}
---
+++ UsersManager.Service/HostUtilities/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using Serilog;
+
+namespace UsersManager.Service.HostUtilities;
+
+public class ExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception e)
+        {
+            Log.Error(e, "Global exception thrown in {Delegate}", _next.Method.Name);
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsync($"Internal exception {nameof(e)} was thrown");
+        }
+    }
+}
