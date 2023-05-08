fbedf8d47101fd6baebccf35f017eaa79a136491
EnergyNick <nsikachin@gmail.com>
2023-05-09 02:27:11 +0500

    Rewrite auth logic in controller, added security

--- UsersManager.Service/Controllers/UsersController.cs
+++ UsersManager.Service/Controllers/UsersController.cs
@@ -1,9 +1,12 @@
+using System.Security.Claims;
 using AutoMapper;
 using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Serilog;
 using UsersManager.Domain.Models;
 using UsersManager.Domain.Services;
+using UsersManager.Service.HostUtilities;
 using UsersManager.Service.Models;
 
 namespace UsersManager.Service.Controllers;
@@ -26,17 +29,20 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<UserDto>> GetUser([FromQuery]string login, CancellationToken token)
+    public async Task<ActionResult<UserDto>> GetUser([FromQuery]string? login, CancellationToken token)
     {
-        if (string.IsNullOrWhiteSpace(login))
-            return BadRequest(OnEmptyIdErrorString);
+        var (validatedLogin, error) = ChooseLogin(login);
 
-        var user = await _userService.GetUser(login, token);
-        if (user is null)
-            return NotFound();
-        return Ok(_mapper.Map<UserDto>(user));
+        if (error is not null)
+            return error;
+
+        var user = await _userService.GetUser(validatedLogin, token);
+        return user is not null
+            ? Ok(_mapper.Map<UserDto>(user))
+            : NotFound();
     }
 
+    [Authorize]
     [HttpGet("all")]
     public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers([FromQuery]int? size, [FromQuery]int? pageIndex,
         CancellationToken token)
@@ -49,11 +55,13 @@ public class UsersController : ControllerBase
     public async Task<ActionResult<UserDto>> CreateUser([FromBody]CreateUserRequestDto requestDto,
         CancellationToken token)
     {
+        const string requestErrorTemplateString = "Bad request to creating user with errors: {Errors}";
+
         var validation = await _validator.ValidateAsync(requestDto, token);
         if (!validation.IsValid)
         {
             var errors = validation.ToString();
-            Log.Information(RequestErrorTemplateString, errors);
+            Log.Information(requestErrorTemplateString, errors);
             return BadRequest(errors);
         }
 
@@ -65,23 +73,51 @@ public class UsersController : ControllerBase
         }
         catch (InvalidOperationException e)
         {
-            Log.Information(RequestErrorTemplateString, e.Message);
+            Log.Information(requestErrorTemplateString, e.Message);
             return BadRequest(e.Message);
         }
         return Ok(_mapper.Map<UserDto>(user));
     }
 
+    [Authorize]
     [HttpDelete]
-    public async Task<ActionResult<UserDto>> DeleteUser([FromQuery]string login,
+    public async Task<ActionResult<UserDto>> DeleteUser([FromQuery]string? login,
         CancellationToken token)
     {
-        if (string.IsNullOrWhiteSpace(login))
-            return BadRequest(OnEmptyIdErrorString);
+        var (validatedLogin, error) = ValidatePrivilegesAndChooseLogin(login);
 
-        var isDeleted = await _userService.DeleteUser(login, token);
+        if (error is not null)
+            return error;
+
+        var isDeleted = await _userService.DeleteUser(validatedLogin, token);
         return isDeleted ? Ok() : NotFound();
     }
 
-    private const string OnEmptyIdErrorString = "User Id must be not empty";
-    private const string RequestErrorTemplateString = "Bad request to creating user with errors: {Errors}";
+    private (string login, ActionResult? error) ChooseLogin(string? loginFromUser)
+    {
+        var nameClaim = HttpContext.User.FindFirstValue(ServiceAuthentication.NameClaimType);
+
+        if (string.IsNullOrWhiteSpace(loginFromUser))
+            loginFromUser = nameClaim;
+
+        return loginFromUser is not null
+            ? (loginFromUser, null)
+            : ("", Unauthorized("Invalid login for operation"));
+    }
+
+    private (string login, ActionResult? error) ValidatePrivilegesAndChooseLogin(string? loginFromUser)
+    {
+        var isAdmin = HttpContext.User.IsInRole(ServiceAuthentication.AdminRole);
+        var nameClaim = HttpContext.User.FindFirstValue(ServiceAuthentication.NameClaimType);
+
+        if (!isAdmin && (nameClaim is null || loginFromUser != nameClaim))
+            return ("", BadRequest("Can't delete other user without admin privileges"));
+
+        if (string.IsNullOrWhiteSpace(loginFromUser))
+            loginFromUser = nameClaim;
+
+        return loginFromUser is not null
+            ? (loginFromUser, null)
+            : ("", Unauthorized("Invalid login for operation"));
+    }
 }
